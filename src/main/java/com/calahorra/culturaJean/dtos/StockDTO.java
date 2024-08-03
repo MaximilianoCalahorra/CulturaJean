b7c32285ab4835e493dbee2f3c7a8adfbde9f548
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2024-08-03 18:38:05 -0300

    Modificación del StockDTO

--- src/main/java/com/calahorra/culturaJean/dtos/StockDTO.java
+++ src/main/java/com/calahorra/culturaJean/dtos/StockDTO.java
@@ -1,5 +1,7 @@
 package com.calahorra.culturaJean.dtos;
 
+import com.calahorra.culturaJean.entities.Product;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,14 +12,16 @@ public class StockDTO
 {
 	//Atributos:
 	private int stockId;
+	private Product product;
 	private int desirableAmount;
 	private int minimumAmount;
 	private int actualAmount;
 	
 	//Constructor:
-	public StockDTO(int StockId, int desirableAmount, int minimumAmount) 
+	public StockDTO(int StockId, Product product, int desirableAmount, int minimumAmount) 
 	{
 		setStockId(stockId);
+		this.product = product;
 		this.desirableAmount = desirableAmount;
 		this.minimumAmount = minimumAmount;
 		this.actualAmount = 0; //Al instanciar un stock su cantidad actual es 0.
