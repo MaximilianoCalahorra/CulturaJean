472f4d36d062582ab89f935d1d463ebf202702e6
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2024-09-08 13:52:52 -0300

    Implementaciones relacionadas a filtros y ordenamientos para los clientes

--- src/main/java/com/calahorra/culturaJean/controllers/CustomerController.java
+++ src/main/java/com/calahorra/culturaJean/controllers/CustomerController.java
@@ -1,10 +1,12 @@
 package com.calahorra.culturaJean.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.calahorra.culturaJean.dtos.MemberDTO;
@@ -17,25 +19,46 @@ import com.calahorra.culturaJean.services.implementation.MemberService;
 public class CustomerController 
 {
 	//Atributo:
-	private MemberService userService;
+	private MemberService memberService;
 	
 	//Constructor:
-	public CustomerController(MemberService userService) 
+	public CustomerController(MemberService memberService) 
 	{
-		this.userService = userService;
+		this.memberService = memberService;
 	}
 	
-	//Respondemos a las peticiones de información sobre los clientes para el administrador:
+	//Respondemos a las peticiones de información sobre los clientes ordenados/filtrados:
 	@GetMapping("/customers")
-	public ModelAndView customers() 
+	public ModelAndView customers(@RequestParam(value = "enabled", defaultValue = "all")String enabled,
+								  @RequestParam(value = "order", defaultValue = "orderAscByLastName")String order) 
 	{
 		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.CUSTOMERS);
 		
-		//Obtenemos los clientes ordenados de forma alfabética por apellido:
-		List<MemberDTO> customers = userService.getByUserRoleInOrderAscByLastName("ROLE_CUSTOMER");
+		List<MemberDTO> customers = new ArrayList<MemberDTO>(); //Instanciamos una lista de miembros para cargarla con los clientes filtrados y/u ordenados.
 		
-		//Agregamos los clientes a la vista:
-		modelAndView.addObject("customers", customers);
+		//Aplicamos el filtro de clientes habilitados/inhabilitados o no:
+		switch(enabled) 
+		{
+			case "all": customers = memberService.findByUserRole("ROLE_CUSTOMER"); break; //Obtenemos todos los clientes.
+			case "true": customers = memberService.findByEnabledAndUserRole(true, "ROLE_CUSTOMER"); break; //Obtenemos los clientes habilitados.
+			case "false": customers = memberService.findByEnabledAndUserRole(false, "ROLE_CUSTOMER"); break; //Obtenemos los clientes deshabilitados.
+		}
+		
+		//Ordenamos el listado de clientes resultante del proceso anterior en base al tipo de ordenamiento elegido:
+		switch(order) 
+		{
+			case "orderAscByName": customers = memberService.inOrderAscByName(customers); break; //Alfabéticamente por nombre.
+			case "orderDescByName": customers = memberService.inOrderDescByName(customers); break; //Inverso al alfabeto por nombre.
+			case "orderAscByLastName": customers = memberService.inOrderAscByLastName(customers); break; //Alfabéticamente por apellido.
+			case "orderDescByLastName": customers = memberService.inOrderDescByLastName(customers); break; //Inverso al alfabeto por apellido.
+			case "orderAscByUsername": customers = memberService.inOrderAscByUsername(customers); break; //Alfabéticamente por nombre de usuario.
+			case "orderDescByUsername": customers = memberService.inOrderDescByUsername(customers); break; //Inverso al alfabeto por nombre de usuario.
+		}
+		
+		//Agregamos la información a la vista:
+		modelAndView.addObject("enabled", enabled); //Indicamos si la lista está filtrada (si es habilitados o inhabilitados) o no.
+		modelAndView.addObject("order", order); //Indicamos por cuál atributo está ordenado el listado y en qué sentido.
+		modelAndView.addObject("customers", customers); //Agregamos los clientes filtrados y/u ordenados a la vista.
 		
 		return modelAndView; //Retornamos la vista con la información adjunta.
 	}
