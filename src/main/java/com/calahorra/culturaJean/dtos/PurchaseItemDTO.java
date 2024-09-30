6d52dddd782787e97b137b13d2e8b847c4ad02c0
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2024-09-30 19:02:06 -0300

    Modifico el DTO para que pueda ser serializable y de esa forma poder incluir sus instancias en el mapa que tiene los ítems de la compra durante la sesión

--- src/main/java/com/calahorra/culturaJean/dtos/PurchaseItemDTO.java
+++ src/main/java/com/calahorra/culturaJean/dtos/PurchaseItemDTO.java
@@ -1,19 +1,29 @@
 package com.calahorra.culturaJean.dtos;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 ///Clase PurchaseItemDTO:
 @Getter @Setter @NoArgsConstructor
-public class PurchaseItemDTO
+public class PurchaseItemDTO implements Serializable
 {
+	private static final long serialVersionUID = 1L;
+	
 	//Atributos:
 	private int purchaseItemId;
 	private ProductDTO product;
 	private int amount;
 	
 	//Constructor:
+	public PurchaseItemDTO(ProductDTO product, int amount) 
+	{
+		this.product = product;
+		this.amount = amount;
+	}
+	
 	public PurchaseItemDTO(int purchaseItemId, ProductDTO product, int amount) 
 	{
 		setPurchaseItemId(purchaseItemId);
