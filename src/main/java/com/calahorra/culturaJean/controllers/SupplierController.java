ef8a6b2e6584e902fe1dadd60eca8b1ab9c2b964
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2024-11-18 22:08:47 -0300

    Refactorización de los pedidos de ordenamiento de los proveedores

--- src/main/java/com/calahorra/culturaJean/controllers/SupplierController.java
+++ src/main/java/com/calahorra/culturaJean/controllers/SupplierController.java
@@ -3,10 +3,11 @@ package com.calahorra.culturaJean.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.calahorra.culturaJean.dtos.SupplierDTO;
@@ -31,6 +32,7 @@ public class SupplierController
 	@GetMapping("/suppliers")
 	public ModelAndView suppliers() 
 	{
+		//Definimos la vista a cargar:
 		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.SUPPLIERS);
 		
 		//Obtenemos todos los proveedores ordenados alfabéticamente por nombre:
@@ -43,6 +45,7 @@ public class SupplierController
 		return modelAndView; //Retornamos la vista con la información adjunta.
 	}
 	
+	/* VERSIÓN ANTERIOR
 	//Respondemos a las solicitudes de filtrado/ordenamiento sobre los proveedores:
     @GetMapping("/suppliers/filter")
     public ModelAndView filteredSuppliers(@RequestParam("order") String order) 
@@ -65,4 +68,25 @@ public class SupplierController
 		
         return modelAndView; //Retornamos el fragmento con los proveedores.
     }
+    */
+	
+	/* NUEVA VERSIÓN */
+	//Respondemos a las solicitudes de filtrado/ordenamiento sobre los proveedores:
+	@GetMapping("/suppliers/{order}")
+	public ResponseEntity<List<SupplierDTO>> orderSuppliers(@PathVariable("order") String order) 
+	{
+		//Instanciamos una lista de proveedores para posteriormente cargarla con los proveedores ordenados:
+		List<SupplierDTO> suppliers = new ArrayList<>(); 
+		
+		//En base al tipo de ordenamiento elegido, obtenemos la lista de proveedores ordenada:
+		switch(order) 
+		{
+			case "orderAscByName": suppliers = supplierService.getAllInOrderAscByName(); break; //Alfabéticamente por nombre.
+			case "orderDescByName": suppliers = supplierService.getAllInOrderDescByName(); break; //Inverso al alfabeto por nombre.
+			case "orderAscBySupplierId": suppliers = supplierService.getAllInOrderAscBySupplierId(); break; //Menor a mayor por id.
+			case "orderDescBySupplierId": suppliers = supplierService.getAllInOrderDescBySupplierId(); break; //Mayor a menor por id.
+		}
+		
+		return ResponseEntity.ok(suppliers); //Retornamos los proveedores ordenados como JSON.
+	}
 }
--- src/main/resources/static/js/suppliers.js
+++ src/main/resources/static/js/suppliers.js
@@ -1,3 +1,60 @@
+/* OBTENEMOS LOS PROVEEDORES ORDENADOS POR EL CRITERIO INDICADO */
+async function orderSuppliers(order)
+{
+	return fetch(`/supplier/suppliers/${order}`,{
+		method: 'GET'		
+	})
+	.then(response => 
+    {
+		//Si hubo algún error:
+        if(!response.ok) 
+        {
+            throw new Error("Error en la respuesta del servidor");
+        }
+        
+        return response.json(); //Retornamos el JSON con los pedidos.
+    })
+    .then(data => data)
+    .catch(error => 
+    {
+        console.error("Error en la solicitud Fetch:", error);
+        throw error;
+    });
+}
+
+/* GENERAMOS EL HTML CON LOS PROVEEDORES INDICADOS */
+function generateHTMLForSuppliers(suppliers)
+{
+	let html = '';
+    suppliers.forEach(supplier => 
+    {
+        html += `<tr>
+        			<td>${supplier.supplierId}</td>
+                    <td>${supplier.name}</td>
+                    <td>${supplier.address}</td>
+                    <td>${supplier.email}</td>
+                    <td>${supplier.phoneNumber}</td>
+                 </tr>`;
+    });
+    return html;	
+}
+
+/* GENERAMOS EL HTML PARA CUANDO NO SE ENCONTRARON PROVEEDORES */
+function generateHTMLForEmptySuppliers()
+{
+	//Obtenemos el body de la tabla:
+	const tbody = document.getElementById("supplierTBodyTable");
+	
+	//Definimos una única fila con el mensaje de que no se encontraron resultados:
+	tbody.innerHTML = 
+	`<tr>
+        <td colspan="5" style="text-align: center; font-style: italic; color: gray;">
+            No se encontraron resultados.
+        </td>
+    </tr>`;	
+}
+
+/* ESCUCHAMOS EL EVENTO DE CARGA DEL DOM */
 //Una vez que el DOM cargó por completo:
 document.addEventListener('DOMContentLoaded', () =>
 {
@@ -7,16 +64,30 @@ document.addEventListener('DOMContentLoaded', () =>
 		//Obtenemos el criterio de ordenamiento elegido:
         const order = document.querySelector("select[name='order']").value;
 
-        //Realizamos la consulta para obtener los proveedores:
-        fetch(`/supplier/suppliers/filter?order=${order}`, {
-            method: 'GET'
-        })
-        //Pasamos la respuesta a texto:
-        .then(response => response.text())
-        .then(data => {
-            //Actualizamos los proveedores con los que obtuvimos:
-            document.getElementById('supplierTable').innerHTML = data;
-        })
-        .catch(error => console.error("Error en la solicitud Fetch:", error));
+		//Obtenemos los proveedores ordenados:
+       	orderSuppliers(order)
+       	.then(suppliers => 
+       	{
+			//Si al menos hay un proveedor:
+			if(suppliers.length > 0)
+			{
+				//Seleccionamos la etiqueta donde van los proveedores:
+				const tbody = document.getElementById("supplierTBodyTable");
+				
+				//Cargamos los proveedores en la vista:
+				tbody.innerHTML = generateHTMLForSuppliers(suppliers);
+			}
+			else
+			{
+				//Generamos el mensaje indicando que no hay proveedores:
+				generateHTMLForEmptySuppliers();
+			}
+		})
+	    .catch(error => 
+	    {
+	        console.error("Ocurrió un error al ordenar los proveedores:", error);
+	    });
     });
 });
+
+
